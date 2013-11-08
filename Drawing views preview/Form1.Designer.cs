d1bee5cd518bdc58eb7f4028207c1fd64378e2f9
Jorma Hopia <jorma.hopia@hopiaengineering.fi>
2013-11-09 01:06:20 +0200

    created

---
+++ Drawing views preview/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Drawing_views_preview
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form1));
+            this.btnPreview2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnPreview2
+            // 
+            this.btnPreview2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPreview2.Location = new System.Drawing.Point(313, 12);
+            this.btnPreview2.Name = "btnPreview2";
+            this.btnPreview2.Size = new System.Drawing.Size(68, 31);
+            this.btnPreview2.TabIndex = 1;
+            this.btnPreview2.Text = "Preview";
+            this.btnPreview2.UseVisualStyleBackColor = true;
+            this.btnPreview2.Click += new System.EventHandler(this.btnPreview2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.panel1.Location = new System.Drawing.Point(6, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(445, 297);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(53, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Scale";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 35);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(465, 346);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(457, 320);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.panel1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(457, 320);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(487, 393);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnPreview2);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(460, 422);
+            this.Name = "form1";
+            this.Text = "Drawing views preview";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPreview2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ Drawing views preview/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Tekla.Structures.Model;
+using Tekla.Structures.Catalogs;
+using Tekla.Structures.Drawing;
+using TSD = Tekla.Structures.Drawing;
+using TS3d = Tekla.Structures.Geometry3d;
+
+namespace Drawing_views_preview
+{
+    public partial class form1 : Form
+    {
+        private Bitmap buffer;
+        
+        public form1()
+        {
+            InitializeComponent();
+            panel1_Resize(this, null); // Initialize buffer 
+            this.StartPosition = FormStartPosition.CenterScreen;
+            //this.MinimumSize = new System.Drawing.Size(297, 210); //A4
+            //this.MaximumSize = new System.Drawing.Size(1189, 841); //A0
+            //this.AutoSize = true; this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+        }
+
+        private void panel1_Resize(object sender, EventArgs e)
+        {
+            // Resize the buffer, if it is growing 
+            if (buffer == null ||
+                buffer.Width < panel1.Width ||
+                buffer.Height < panel1.Height)
+            {
+                Bitmap newBuffer = new Bitmap(panel1.Width, panel1.Height);
+                if (buffer != null)
+                    using (Graphics bufferGrph = Graphics.FromImage(newBuffer))
+                        bufferGrph.DrawImageUnscaled(buffer, Point.Empty);
+                buffer = newBuffer;
+            }
+        }
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            // Draw the buffer into the panel 
+            e.Graphics.DrawImageUnscaled(buffer, Point.Empty);
+        }
+
+
+        private void btnPreview2_Click(object sender, EventArgs e)
+        {
+            // Draw into the buffer when button is clicked 
+            PaintBlueRectangle();
+        }
+
+
+        private void PaintBlueRectangle()
+        {
+            ArrayList ItemList = new ArrayList();
+            Drawing ActivatedDrawing;
+            bool DrawingFound = false;
+
+            DrawingHandler MyDrawingHandler = new DrawingHandler();
+
+            if (MyDrawingHandler.GetConnectionStatus())
+            {
+                DrawingEnumerator SelectedDrawings = MyDrawingHandler.GetDrawingSelector().GetSelected();
+
+                while (SelectedDrawings.MoveNext() && !DrawingFound)
+                {
+                    ActivatedDrawing = SelectedDrawings.Current;
+                    double sheetHeight = ActivatedDrawing.Layout.SheetSize.Height;
+                    double sheetWidth = ActivatedDrawing.Layout.SheetSize.Width;
+                   
+                    double minX = 0.0; 
+                    double minY = 0.0; 
+                    double maxX = sheetWidth; 
+                    double maxY = sheetHeight;
+                    
+                    TSD.ContainerView currentSheet = ActivatedDrawing.GetSheet();
+                    TSD.DrawingObjectEnumerator viewEnum = currentSheet.GetViews();
+                    while (viewEnum.MoveNext())
+                    {
+                        TSD.View currentView = viewEnum.Current as TSD.View;
+                        RectangleBoundingBox ViewAABB = currentView.GetAxisAlignedBoundingBox();
+                        double rbbx = ViewAABB.UpperLeft.X;
+                        double rbby = ViewAABB.UpperLeft.Y;
+                        double rbbW = ViewAABB.Width;
+                        double rbbH = ViewAABB.Height;
+                        double viewScale = currentView.Attributes.Scale;
+                        //MessageBox.Show("rbbx=" + Convert.ToString(rbbx) + ", rbby=" + Convert.ToString(rbby) + ", rbbW=" + Convert.ToString(rbbW) + ", rbbH=" + Convert.ToString(rbbH));
+                        ItemList.Add(rbbx);
+                        ItemList.Add(rbby);
+                        ItemList.Add(rbbW);
+                        ItemList.Add(rbbH);
+                        ItemList.Add(viewScale);
+                        
+                        if (ViewAABB.UpperLeft.X < minX)
+                        {
+                            minX = ViewAABB.UpperLeft.X;
+                        }
+                        if (ViewAABB.LowerLeft.Y < minY)
+                        {
+                            minY = ViewAABB.LowerLeft.Y;
+                        }
+                        if (ViewAABB.UpperRight.X > maxX)
+                        {
+                            maxX = ViewAABB.UpperRight.X;                           
+                        }
+                        if (ViewAABB.UpperLeft.Y > maxY)
+                        {
+                            maxY = ViewAABB.UpperLeft.Y;
+                        }
+                    }
+
+                    double bufferWidth = Math.Abs(minX) + maxX;
+                    double bufferHeight = Math.Abs(minY) + maxY;
+
+                    //panel1.Width = (int)Convert.ToInt32(bufferWidth);
+                    //panel1.Height = (int)Convert.ToInt32(bufferHeight);
+                    //btnPreview2.Location = new Point(panel1.Width - btnPreview2.Size.Width - 15, panel1.Height + btnPreview2.Size.Height);
+                    //checkBox1.Location = new Point((int)Convert.ToInt32(0.2 * panel1.Width), panel1.Height + checkBox1.Size.Height + 20);
+                    this.Size = new System.Drawing.Size(panel1.Width + 20, panel1.Height + btnPreview2.Size.Height + 20);
+                    buffer = new Bitmap((int)Convert.ToInt32(bufferWidth), (int)Convert.ToInt32(bufferHeight));
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+                    /*
+                                   double scaleFactorX = 297 / bufferWidth;
+                                   double scaleFactorY = 210 / bufferHeight;
+                                   double scaleFactor = 0;
+                                   if (scaleFactorX <= scaleFactorY)
+                                   {
+                                       scaleFactor = scaleFactorX;
+                                   }else{
+                                       scaleFactor=scaleFactorY;
+                                   }
+               
+
+                                                       panel1.Width = (int)Convert.ToInt32(bufferWidth * scaleFactor);
+                                                       panel1.Height = (int)Convert.ToInt32(bufferHeight * scaleFactor);                   
+                                                       btnPreview2.Location = new Point(panel1.Width - btnPreview2.Size.Width - 15, panel1.Height + btnPreview2.Size.Height);
+                                                       checkBox1.Location = new Point((int)Convert.ToInt32(0.2*panel1.Width), panel1.Height + checkBox1.Size.Height + 20);
+                                                       this.Size = new System.Drawing.Size(panel1.Width + 20, panel1.Height + btnPreview2.Size.Height + 20);
+                                                       buffer = new Bitmap((int)Convert.ToInt32(bufferWidth * scaleFactor), (int)Convert.ToInt32(bufferHeight * scaleFactor));
+                                      */
+ //////////////////////////////////////////////////////////////////////////////////////
+
+                    
+
+                    // Draw blue rectangle into the buffer 
+                    using (Graphics bufferGrph = Graphics.FromImage(buffer))
+                    {
+                        Pen drwPen = new Pen(Color.DarkBlue, 4);
+                        bufferGrph.Clear(Color.White);
+                        System.Drawing.Rectangle myRectangle = new System.Drawing.Rectangle((int)Convert.ToInt32(Math.Abs(minX)), (int)Convert.ToInt32(Math.Abs(maxY)-sheetHeight), (int)Convert.ToInt32(sheetWidth), (int)Convert.ToInt32(sheetHeight));
+                       
+                        bufferGrph.DrawRectangle(drwPen, myRectangle);
+
+                        for (int i = 0; i <= ItemList.Count / 5 - 1; i++)
+                        {
+                            int w = (int)Convert.ToInt32(ItemList[5 * i + 2]);
+                            int h = (int)Convert.ToInt32(ItemList[5 * i + 3]) ;
+                            int x = (int)Convert.ToInt32(ItemList[5 * i]) + (int)Convert.ToInt32(Math.Abs(minX));
+                            int y = (int)Convert.ToInt32(Math.Abs(maxY) - Convert.ToDouble(ItemList[5 * i + 1]));
+                            bufferGrph.DrawRectangle(new Pen(Color.Blue, 2), x, y, w, h);
+
+                            if (checkBox1.Checked)
+                            {
+                                int sx, sy;
+                                // Create string to draw.
+                                String drawString = "Scale:" + Convert.ToString(ItemList[5 * i + 4]);
+                                // Create font and brush.
+                                Font drawFont = new Font("Arial", 8);
+                                SolidBrush drawBrush = new SolidBrush(Color.Black);
+                                // Set format of string.
+                                StringFormat drawFormat = new StringFormat();
+                                if (h > w)
+                                {
+                                    // Create point for upper-left corner of drawing.
+                                    sx = x + (int)(0.5 * w); sy = y + (int)(0.25 * h);
+                                    drawFormat.FormatFlags = StringFormatFlags.DirectionVertical;
+                                }
+                                else
+                                {
+                                    // Create point for upper-left corner of drawing.
+                                    sx = x + (int)(0.25 * w); sy = y + (int)(0.5 * h);
+                                    drawFormat.FormatFlags = StringFormatFlags.DisplayFormatControl;
+                                }
+                                // Draw string to screen.
+                                bufferGrph.DrawString(drawString, drawFont, drawBrush, sx, sy, drawFormat);
+                            }
+                        }
+                        //Be sure the dispose of the pench
+                        drwPen.Dispose();
+                    }
+                    // Invalidate the panel. This will lead to a call of 'panel1_Paint' 
+                    panel1.Invalidate();
+                    //only one loop
+                    DrawingFound = true;
+                }
+            }
+        }
+
+        private void form1_Load(object sender, EventArgs e) { }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) { }
+    }
+}
